fe500451cf2aecd7cb6d0ad0539e18cd93d1b931
yosefzonana <54564641+yosefzonana@users.noreply.github.com>
2020-05-14 03:34:17 -0500

    Modificacion de sw

--- js/app.js
+++ js/app.js
@@ -1,6 +1,16 @@
 //REFERENCIA SW
+
+//REASIGNACION DE UBICACIONES DEL SW
+var url = window.location.href;
+var swLocation='/twittor/sw.js';
+
 if (navigator.serviceWorker) {
-    navigator.serviceWorker.register('/sw.js');
+
+    if (url.includes('localhost')) {
+        swLocation='/sw.js';
+    }
+
+    navigator.serviceWorker.register(swLocation);
 }
 
 
--- sw.js
+++ sw.js
@@ -9,25 +9,25 @@ const DYNAMIC_CACHE= 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL = [
-	'/',
+	// '', SOLO PRODUCCION
 	'/index.html',
 	'css/style.css',
-	'/img/favicon.ico',
-	'/img/avatars/spiderman.jpg',
-	'/img/avatars/ironman.jpg',
-	'/img/avatars/wolverine.jpg',
-	'/img/avatars/thor.jpg',
-	'/img/avatars/hulk.jpg',
-	'/js/app.js',
-	'/js/sw-utils.js'
+	'img/favicon.ico',
+	'img/avatars/spiderman.jpg',
+	'img/avatars/ironman.jpg',
+	'img/avatars/wolverine.jpg',
+	'img/avatars/thor.jpg',
+	'img/avatars/hulk.jpg',
+	'js/app.js',
+	'js/sw-utils.js'
 ]
 
 const APP_SHELL_INMUTABLE=[
 	'https://fonts.googleapis.com/css?family=Quicksand:300,400',
 	'https://fonts.googleapis.com/css?family=Lato:400,300',
 	'https://use.fontawesome.com/releases/v5.3.1/css/all.css',
-	'/css/animate.css',
-	'/js/libs/jquery.js'
+	'css/animate.css',
+	'js/libs/jquery.js'
 ]
 
 
